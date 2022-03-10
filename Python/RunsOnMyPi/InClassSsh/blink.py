393848e6ca4e05a080fa461e6903826e1946d96f
David Fisher <fisherds@rose-hulman.edu>
2022-03-10 15:18:36 -0500

    finished LED blink

--- Python/RunsOnMyPi/InClassSsh/blink.py
+++ Python/RunsOnMyPi/InClassSsh/blink.py
@@ -1,1 +1,18 @@
-print("Ready to blink!!!!!!!!")
+import gpiozero as gz
+import time
+
+def main():
+    print("Ready to blink via main")
+    my_led = gz.LED(18)
+
+    while True:
+        my_led.on()
+        # print("The LED should be on")
+        time.sleep(1)
+        my_led.off()
+        # print("The LED should be off")
+        time.sleep(1)
+
+
+
+main()
