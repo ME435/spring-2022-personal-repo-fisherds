dd6ade9d13bb883ae233e879ea382f163745ec86
Dave Fisher <fisherds@gmail.com>
2022-03-14 15:39:32 -0400

    working pigpio

--- Node/discoveringAPIs/using_pigpio/blink.js
+++ Node/discoveringAPIs/using_pigpio/blink.js
@@ -1,4 +1,6 @@
-// Imports
+const Gpio = require('pigpio').Gpio;
+
+
 function msleep(n) {
     Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
 }
@@ -9,15 +11,18 @@ function main() {
     console.log("Ready");
 
     // TODO: Setup GPIO 14 as an output.
+    const led = new Gpio(14, {mode: Gpio.OUTPUT});
 
     while (true) {
         // TODO: Turn GPIO 14 on
         console.log("LED On");
+        led.digitalWrite(1);
         sleep(1);
 
 
         // TODO: Turn GPIO 14 off
         console.log("LED Off");
+        led.digitalWrite(0);
         sleep(1);
 
     }
