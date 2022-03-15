c711613f3654548940b3644afe5d19375f3c593e
Dave Fisher <fisherds@gmail.com>
2022-03-15 15:24:36 -0400

    finish rpio blink

--- Node/discoveringAPIs/node_led_blink/using_rpio/blink.js
+++ Node/discoveringAPIs/node_led_blink/using_rpio/blink.js
@@ -1,26 +1,18 @@
 // Imports
-
-function msleep(n) {
-    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
- }
- 
- function sleep(n) {
-    msleep(n * 1000);
- }
+var rpio = require('rpio');
 
 function main() {
     console.log("Ready");
-
-    // TODO: Set GPIO 14 as an output
-
+    let gpio14 = 8;
+    rpio.open(gpio14, rpio.OUTPUT, rpio.LOW);
     while (true) {
         console.log("LED On");
-        // TODO: Turn the LED on using a library
-        sleep(1);
+        rpio.write(gpio14, rpio.HIGH);
+        rpio.sleep(1);
 
         console.log("LED Off");
-        // TODO: Turn the LED off using a library
-        sleep(1);
+        rpio.write(gpio14, rpio.LOW);
+        rpio.sleep(1);
     }
 }
 
