7d65f491f5a532f22df6f71ee16910b164ed8f7b
Dave Fisher <fisherds@gmail.com>
2022-03-17 14:47:46 -0400

    npm install prompt-sync

---
+++ Node/discoveringAPIs/missing_node_input_command/node_input.js
@@ -0,0 +1,12 @@
+function main() {
+    console.log("Ready");
+    
+    while (true) {
+        var name = input("What is your name? ");
+        
+        console.log(`Hello, ${name}!`);
+    }
+
+}
+
+main();
