5df1caf937aba4b9599b516fcd3545ee64690a2a
Dave Fisher <fisherds@gmail.com>
2022-03-17 14:52:42 -0400

    node equivalent to python input

--- Node/discoveringAPIs/missing_node_input_command/node_input.js
+++ Node/discoveringAPIs/missing_node_input_command/node_input.js
@@ -1,12 +1,16 @@
+var prompt = require('prompt-sync')();
+
 function main() {
     console.log("Ready");
     
     while (true) {
-        var name = input("What is your name? ");
-        
+        var name = prompt("What is your name? ");
+        if (!name) {
+            console.log("Goodbye");
+            break;
+        }
         console.log(`Hello, ${name}!`);
     }
-
 }
 
 main();
