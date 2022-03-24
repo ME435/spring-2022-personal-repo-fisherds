de097b7d45e29eeff1924fc914eadf2c873491b6
David Fisher <fisherds@rose-hulman.edu>
2022-03-24 14:29:51 -0400

    simple Arduino com program

--- Arduino/sketches/simple_com/simple_com.ino
+++ Arduino/sketches/simple_com/simple_com.ino
@@ -4,14 +4,23 @@ bool stringComplete = false;
 void setup() {
   Serial.begin(9600);
   inputString.reserve(200);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   if (stringComplete) {
-    Serial.print("From Arduino --> ");
-    Serial.println(inputString);
 
     // TODO: Do something real with the message!
+    if (inputString.equalsIgnoreCase("ON")) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.println("The LED is now on!");
+    } else if (inputString.equalsIgnoreCase("OFF")) {
+      digitalWrite(LED_BUILTIN, LOW);
+      Serial.println("The LED is now off!");
+    } else {
+      Serial.print("From Arduino --> ");
+      Serial.println(inputString);
+    }
 
     // clear the string:
     inputString = "";
