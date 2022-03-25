2f87c429ef25da5433b464b14838bef20cb4a4fe
Dave Fisher <fisherds@gmail.com>
2022-03-25 10:52:24 -0400

    passing path parameters

--- Node/GPIOServeVideos/app.js
+++ Node/GPIOServeVideos/app.js
@@ -1,12 +1,88 @@
-const express = require("express")
+const express = require("express");
+const Gpio = require("pigpio").Gpio;
 
 const app = express();
 app.use("/", express.static("public"));
+
+// Pin setup
+const ledRed = new Gpio(14, {
+    mode: Gpio.OUTPUT
+});
+const ledYellow = new Gpio(15, {
+    mode: Gpio.OUTPUT
+});
+const ledGreen = new Gpio(18, {
+    mode: Gpio.OUTPUT
+});
+const pushbutton = new Gpio(25, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_UP,
+    edge: Gpio.FALLING_EDGE
+});
+
 
 app.get("/hello", (req, res) => {
     res.json({
         status: "ok",
         message: "Hello World"
+    });
+});
+
+app.get("/api/ledon", (req, res) => {
+    console.log("Turning the LED On");
+    ledRed.digitalWrite(1);
+    ledYellow.digitalWrite(1);
+    ledGreen.digitalWrite(1);
+    res.json({
+        status: "ok",
+        message: "LED On"
+    });
+});
+
+app.get("/api/ledoff", (req, res) => {
+    console.log("Turning the LED Off");
+    ledRed.digitalWrite(0);
+    ledYellow.digitalWrite(0);
+    ledGreen.digitalWrite(0);
+    res.json({
+        status: "ok",
+        message: "LED Offf"
+    });
+});
+
+function setLed(color, value) {
+
+    switch (color) {
+        case "r":
+            ledRed.digitalWrite(value);
+            break;
+        case "y":
+            ledYellow.digitalWrite(value);
+            break;
+        case "g":
+            ledGreen.digitalWrite(value);
+            break;
+    }
+
+}
+
+app.get("/api/ledon/:color", (req, res) => {
+    const color = req.params.color;
+    setLed(color, 1);
+    console.log("Turning the LED On for color " + color);
+    res.json({
+        status: "ok",
+        message: `LED ${color} On`
+    });
+});
+
+app.get("/api/ledoff/:color", (req, res) => {
+    const color = req.params.color;
+    console.log("Turning the LED Off for " + color);
+    setLed(color, 0);
+    res.json({
+        status: "ok",
+        message: `LED ${color} Off`
     });
 });
 
--- Node/GPIOServeVideos/public/scripts/main.js
+++ Node/GPIOServeVideos/public/scripts/main.js
@@ -3,20 +3,38 @@ var rhit = rhit || {};
 rhit.ViewController = class {
 	constructor() {
 		console.log("Add button listeners");
-		document.querySelector("#ledOnButton").onclick = (event) => {
-			this.handleLedOn();
+		document.querySelector("#redLedOnButton").onclick = (event) => {
+			this.handleLedOn("r");
 		}
-		document.querySelector("#ledOffButton").onclick = (event) => {
-			this.handleLedOff();
+		document.querySelector("#redLedOffButton").onclick = (event) => {
+			this.handleLedOff("r");
+		}
+		document.querySelector("#yellowLedOnButton").onclick = (event) => {
+			this.handleLedOn("y");
+		}
+		document.querySelector("#yellowLedOffButton").onclick = (event) => {
+			this.handleLedOff("y");
+		}
+		document.querySelector("#greenLedOnButton").onclick = (event) => {
+			this.handleLedOn("g");
+		}
+		document.querySelector("#greenLedOffButton").onclick = (event) => {
+			this.handleLedOff("g");
 		}
 	}
 
-	handleLedOn() {
-		console.log("You clicked the On button");
+	async handleLedOn(color) {
+		console.log("You clicked the On button for " + color);
+		const response = await fetch("/api/ledon/" + color);
+		const data = await response.json();
+		console.log("Response", data);
 	}
 
-	handleLedOff() {
-		console.log("You clicked the Off button");
+	async handleLedOff(color) {
+		console.log("You clicked the Off button for " + color);
+		const response = await fetch("/api/ledoff/" + color);
+		const data = await response.json();
+		console.log("Response", data);
 	}
 }
 
